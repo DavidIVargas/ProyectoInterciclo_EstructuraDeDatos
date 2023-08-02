9763786b30f119584fe13b5d1c3354fae4cf0045
davidvargas <davidvargas@localhost>
2023-08-01 20:10:11 -0500

    Trabajo final de estructura de datos

---
+++ src/main/java/ec/edu/ups/est/borrador/controlador/ArbolContactos.java
@@ -0,0 +1,159 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.est.borrador.controlador;
+import ec.edu.ups.est.borrador.modelo.Contacto;
+import ec.edu.ups.est.borrador.modelo.Nodo;
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ *
+ * @author jeanp
+ */
+public class ArbolContactos {
+    private Nodo raiz;
+
+    public ArbolContactos() {
+        this.raiz = null;
+    }
+
+    public void agregarContacto(Contacto contacto) {
+        raiz = agregarContactoRecursivo(raiz, contacto);
+    }
+
+    private Nodo agregarContactoRecursivo(Nodo nodo, Contacto contacto) {
+        if (nodo == null) {
+            return new Nodo(contacto);
+        }
+
+        if (contacto.getNombre().compareTo(nodo.getContacto().getNombre()) < 0) {
+            nodo.izquierdo = agregarContactoRecursivo(nodo.izquierdo, contacto);
+        } else if (contacto.getNombre().compareTo(nodo.getContacto().getNombre()) > 0) {
+            nodo.derecho = agregarContactoRecursivo(nodo.derecho, contacto);
+        }
+
+        return nodo;
+    }
+
+    public Contacto buscarContacto(String nombre) {
+        return buscarContactoRecursivo(raiz, nombre);
+    }
+
+    private Contacto buscarContactoRecursivo(Nodo nodo, String nombre) {
+        if (nodo == null) {
+            return null;
+        }
+
+        if (nombre.equals(nodo.getContacto().getNombre())) {
+            return nodo.getContacto();
+        } else if (nombre.compareTo(nodo.getContacto().getNombre()) < 0) {
+            return buscarContactoRecursivo(nodo.izquierdo, nombre);
+        } else {
+            return buscarContactoRecursivo(nodo.derecho, nombre);
+        }
+    }
+
+    public boolean eliminarContacto(String nombre) {
+        if (raiz == null) {
+            return false;
+        }
+
+        Nodo nodoEliminado = eliminarContactoRecursivo(raiz, nombre);
+        return nodoEliminado != null;
+    }
+
+    private Nodo eliminarContactoRecursivo(Nodo nodo, String nombre) {
+    if (nodo == null) {
+        return null;
+    }
+
+    if (nombre.compareTo(nodo.getContacto().getNombre()) < 0) {
+        nodo.izquierdo = eliminarContactoRecursivo(nodo.izquierdo, nombre);
+    } else if (nombre.compareTo(nodo.getContacto().getNombre()) > 0) {
+        nodo.derecho = eliminarContactoRecursivo(nodo.derecho, nombre);
+    } else {
+        // Caso 1: Nodo sin hijos o con un solo hijo
+        if (nodo.izquierdo == null) {
+            return nodo.derecho;
+        } else if (nodo.derecho == null) {
+            return nodo.izquierdo;
+        }
+
+        // Caso 2: Nodo con dos hijos
+        Nodo sucesor = encontrarSucesor(nodo.derecho);
+        nodo.getContacto().setNombre(sucesor.getContacto().getNombre());
+        nodo.getContacto().setTelefono(sucesor.getContacto().getTelefono());
+        nodo.derecho = eliminarContactoRecursivo(nodo.derecho, sucesor.getContacto().getNombre());
+    }
+
+    return nodo;
+}
+
+
+    private Nodo encontrarSucesor(Nodo nodo) {
+        while (nodo.izquierdo != null) {
+            nodo = nodo.izquierdo;
+        }
+        return nodo;
+    }
+
+    public void mostrarPreorder() {
+        mostrarPreorderRecursivo(raiz);
+    }
+
+    private void mostrarPreorderRecursivo(Nodo nodo) {
+        if (nodo != null) {
+            System.out.println(nodo.getContacto().getNombre());
+            mostrarPreorderRecursivo(nodo.izquierdo);
+            mostrarPreorderRecursivo(nodo.derecho);
+        }
+    }
+
+    public void mostrarInorder() {
+        mostrarInorderRecursivo(raiz);
+    }
+
+    private void mostrarInorderRecursivo(Nodo nodo) {
+        if (nodo != null) {
+            mostrarInorderRecursivo(nodo.izquierdo);
+            System.out.println(nodo.getContacto().getNombre());
+            mostrarInorderRecursivo(nodo.derecho);
+        }
+    }
+
+    public void mostrarPostorder() {
+        mostrarPostorderRecursivo(raiz);
+    }
+
+    private void mostrarPostorderRecursivo(Nodo nodo) {
+        if (nodo != null) {
+            mostrarPostorderRecursivo(nodo.izquierdo);
+            mostrarPostorderRecursivo(nodo.derecho);
+            System.out.println(nodo.getContacto().getNombre());
+        }
+    }
+
+    public void mostrarAnchura() {
+        if (raiz == null) {
+            return;
+        }
+
+        Queue<Nodo> cola = new LinkedList<>();
+        cola.add(raiz);
+
+        while (!cola.isEmpty()) {
+            Nodo nodo = cola.poll();
+            System.out.println(nodo.getContacto().getNombre());
+
+            if (nodo.izquierdo != null) {
+                cola.add(nodo.izquierdo);
+            }
+
+            if (nodo.derecho != null) {
+                cola.add(nodo.derecho);
+            }
+        }
+    }
+    
+}
---
+++ src/main/java/ec/edu/ups/est/borrador/modelo/Contacto.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.est.borrador.modelo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author jeanp
+ */
+public class Contacto {
+
+    private String nombre;
+    private String telefono;
+    private List<String> correos;
+    private Map<String, String> redesSociales;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correos = new ArrayList<>();
+        this.redesSociales = new HashMap<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public List<String> getCorreos() {
+        return correos;
+    }
+
+    public Map<String, String> getRedesSociales() {
+        return redesSociales;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreos(List<String> correos) {
+        this.correos = correos;
+    }
+
+    public void setRedesSociales(Map<String, String> redesSociales) {
+        this.redesSociales = redesSociales;
+    }
+
+    public void agregarCorreo(String correo) {
+        correos.add(correo);
+    }
+
+    public void agregarRedSocial(String redSocial, String perfil) {
+        redesSociales.put(redSocial, perfil);
+    }
+
+    public String obtenerInformacionContacto() {
+        StringBuilder info = new StringBuilder();
+        info.append("Nombre: ").append(nombre).append("\n");
+        info.append("Teléfono: ").append(telefono).append("\n");
+
+        if (!correos.isEmpty()) {
+            info.append("Correos electrónicos:\n");
+            for (String correo : correos) {
+                info.append("- ").append(correo).append("\n");
+            }
+        }
+
+        if (!redesSociales.isEmpty()) {
+            info.append("Redes sociales:\n");
+            for (Map.Entry<String, String> entry : redesSociales.entrySet()) {
+                info.append("- ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+            }
+        }
+
+        return info.toString();
+    }
+
+    @Override
+    public String toString() {
+        return obtenerInformacionContacto();
+    }
+
+}
